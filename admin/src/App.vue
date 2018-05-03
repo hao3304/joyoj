b94dedd5e050688accc93442b6e95b2b1015e07f
hao3304 <hao3304@qq.com>
2018-05-03 14:58:03 +0800

    init

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -1,50 +1,26 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-
-
-    <div>
-     username: <input v-model="form.username " />
-      <br>
-      password: <input v-model="form.password" />
-
-      <Button @click="onLogin">登录</Button>
-
+    <div id="main" class="app-main">
+        <router-view></router-view>
     </div>
-  </div>
 </template>
 
 <script>
-  import axios from 'axios'
-export default {
-  name: 'App',
-  data() {
-    return {
-      form: {
-        username: '',
-        password: ''
-      }
-    }
-  },
-  methods: {
-    onLogin() {
-      debugger
-      axios.post('/admin/login', this.form).then(rep=>{
-        debugger
-      })
-    }
-  }
-}
+    export default {
+        mounted () {
+
+        },
+        beforeDestroy () {
+
+        },
+        methods: {
+
+        }
+    };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.app-main{
+    width: 100%;
+    height: 100%;
 }
 </style>
--- admin/src/main.js
+++ admin/src/main.js
@@ -1,15 +1,18 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from 'vue';
+import iView from 'iview';
+import {router} from './router/index';
+import store from './store';
+import App from './App.vue';
+import 'iview/dist/styles/iview.css';
 
-Vue.config.productionTip = false
+Vue.use(iView);
 
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
-})
+    el: '#app',
+    router: router,
+    store: store,
+    render: h => h(App),
+    mounted () {
+        this.$store.commit('updateMenulist');
+    }
+});
